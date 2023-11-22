390b4d43134f275769a97df47d20b5422c5ac589
oojoo <hosje7324@gmail.com>
2023-11-22 19:23:59 +0900

    first xml merge done

---
+++ app/src/main/java/com/example/picssu/Calendar_click.java
@@ -0,0 +1,14 @@
+package com.example.picssu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Calendar_click extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_click);
+    }
+}
