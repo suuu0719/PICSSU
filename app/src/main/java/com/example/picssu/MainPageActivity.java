ccbcb75ef3baea6f9a939b557acd88c8552adc91
inbin20 <yougsukix@naver.com>
2023-11-24 18:04:37 +0900

    inbin

--- app/src/main/java/com/example/picssu/MainPageActivity.java
+++ app/src/main/java/com/example/picssu/MainPageActivity.java
@@ -1,12 +1,8 @@
 package com.example.picssu;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.picssu.databinding.ActivityMainpageBinding;
-import com.example.picssu.databinding.ActivityMypageBinding;
 
 
 public class MainPageActivity extends AppCompatActivity {
@@ -14,16 +10,6 @@ public class MainPageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        ActivityMainpageBinding binding = ActivityMainpageBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        binding.mypage.setOnClickListener(v -> { //마이페이지 실행
-            startActivity(new Intent(MainPageActivity.this, MypageActivity.class));
-        });
-
-        binding.calender.setOnClickListener(v -> { //캘린더 실행
-            startActivity(new Intent(MainPageActivity.this, Calendar.class));
-        });
+        setContentView(R.layout.activity_mainpage);
     }
 }
